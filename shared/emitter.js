62d001230d5707cd0c99c32fea16054a5bf52ff6
Matt Carter <m@ttcarter.com>
2022-02-17 10:07:24 +1000

    refactor: Switched from standard EventEmitter -> Mitt to maintain support in browser

--- shared/emitter.js
+++ shared/emitter.js
@@ -1,9 +1,21 @@
-import EventEmitter from 'node:events';
+import Mitt from 'mitt';
 
 /**
 * Generic wrapper for an event emitter
-* This module returns a wrapped version of the NodeJS emitter with the intention that it will be replaced for browser versions
+* This module returns a wrapped version of `mitt` stand-alone event emitter (+ support for method chaining)
 */
 export default function emitter() {
-	return new EventEmitter;
+	let emitter = Mitt();
+
+	// Add method chaining
+	emitter.mitt = {};
+	['on', 'off', 'emit'].forEach(f => {
+		emitter.mitt[f] = emitter[f]; // Backup old function into `.mitt.${F}`
+		emitter[f] = (...args) => {
+			emitter.mitt[f](...args);
+			return emitter;
+		};
+	});
+
+	return emitter;
 }
