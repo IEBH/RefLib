331513eb1cd7b84aa56144c55f91f3569950f812
Matt Carter <m@ttcarter.com>
2022-04-14 10:33:20 +1000

    refactor: Move from NPM bfj -> JSONStream

--- modules/json.js
+++ modules/json.js
@@ -1,5 +1,5 @@
 import Emitter from '../shared/emitter.js';
-import bfjc from 'bfj-collections';
+import JSONStream from 'JSONStream';
 
 /**
 * @see modules/interface.js
@@ -10,13 +10,15 @@ export function readStream(stream) {
 
 	// Queue up the parser in the next tick (so we can return the emitter first)
 	setTimeout(()=>
-		bfjc(stream, {pause: false})
-			.on('bfjc', ref => emitter.emit('ref', {
-				recNumber: recNumber++,
-				...ref,
-			}))
-			.on('end', ()=> emitter.emit('end'))
-			.on('error', emitter.emit.bind('error'))
+		stream.pipe(
+			JSONStream.parse('*')
+				.on('data', ref => emitter.emit('ref', {
+					recNumber: recNumber++,
+					...ref,
+				}))
+				.on('end', ()=> emitter.emit('end'))
+				.on('error', emitter.emit.bind('error'))
+		)
 	);
 
 	return emitter;
