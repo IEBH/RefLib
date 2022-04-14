42af74791361f570d58d01c269c653613f2b40e4
Matt Carter <m@ttcarter.com>
2022-04-14 10:42:04 +1000

    fix: Removed mooted Vite plugins to monkey-patch BFJ

--- test/browser/vite.config.js
+++ test/browser/vite.config.js
@@ -3,30 +3,16 @@ import vue from '@vitejs/plugin-vue';
 import {babel} from '@rollup/plugin-babel';
 import {NodeGlobalsPolyfillPlugin} from '@esbuild-plugins/node-globals-polyfill';
 import {NodeModulesPolyfillPlugin} from '@esbuild-plugins/node-modules-polyfill';
-import {replaceCodePlugin as replaceCode} from 'vite-plugin-replace';
 import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
 
 export default defineConfig({
 	plugins: [
 		babel(), // Config for babel in package.json
 		vue(),
-		replaceCode({
-			replacements: [
-				{ // Needed to monkey patch the stupid BFJ assesion checker out so it works in the browser with non stream.Readable instances
-					from: 'check.assert.instanceStrict',
-					to: 'console.log',
-				},
-				{ // Needed to monkey patch BFJ (again) because setImmediate is a Node thing only
-					from: 'setImmediate(',
-					to: 'setTimeout(',
-				},
-			],
-		}),
 	],
 	resolve: {
 		alias: {
 			// reflib: '../..', // FIXME: Vite doesn't support relative path resolution yet, when it does uncomment + correct import in `src/components/reflibTest.vue`
-			util: 'rollup-plugin-node-polyfills/polyfills/util.js', // Needed to monkey patch util.inherits into BFJ
 		},
 	},
 	optimizeDeps: {
@@ -36,14 +22,13 @@ export default defineConfig({
             },
             plugins: [
                 NodeGlobalsPolyfillPlugin({
-                    process: true,
-                    buffer: false,
+                    buffer: true,
+                    process: false,
                 }),
                 NodeModulesPolyfillPlugin(),
             ],
         },
     },
-	/*
     build: {
         rollupOptions: {
             plugins: [
@@ -52,5 +37,4 @@ export default defineConfig({
             ],
         },
     },
-	*/
 })
