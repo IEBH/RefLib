2e28fe90e11c96c3e5c740a7f3665432977d9034
Connor Forbes <connorforbes25@gmail.com>
2024-06-05 20:30:23 +1000

    feat: Native JSON parsing

--- modules/json.js
+++ modules/json.js
@@ -1,6 +1,43 @@
 import Emitter from '../shared/emitter.js';
 import JSONStream from 'JSONStream';
 
+class BrowserJSONStream {
+	constructor() {
+			this.text = '';
+			this.emitter = Emitter();
+	}
+
+	write(data) {
+		// CF: TODO: Parse data as it comes in chunks for better memory efficiency
+		this.text += data
+	}
+
+	end() {
+		try {
+			// Parse this.text as JSON
+			const jsonArray = JSON.parse(this.text);
+
+			// For each entry in the json array (as ref):
+			jsonArray.forEach(ref => {
+				this.emitter.emit('ref', {
+					recNumber: this.recNumber++,
+					...ref,
+				});
+			});
+
+			// Finished
+			this.emitter.emit('end');
+		} catch (e) {
+			console.error('Error parsing final JSON:', e);
+			this.emitter.emit('error', e);
+		}
+	}
+
+	on(event, listener) {
+		this.emitter.on(event, listener);
+	}
+}
+
 /**
 * @see modules/interface.js
 */
@@ -11,15 +48,36 @@ export function readStream(stream) {
 	// Queue up the parser in the next tick (so we can return the emitter first)
 	setTimeout(()=> {
 		stream.on('data', ()=> emitter.emit('progress', stream.bytesRead));
-		stream.pipe(
-			JSONStream.parse('*')
-				.on('data', ref => emitter.emit('ref', {
-					recNumber: recNumber++,
-					...ref,
-				}))
-				.on('end', ()=> emitter.emit('end'))
-				.on('error', emitter.emit.bind('error'))
-		)
+
+		if (stream.isBrowser === true) {
+			// On browser
+			console.log('Parsing JSON natively in browser');
+			const browserJSONStream = new BrowserJSONStream();
+			browserJSONStream.on('ref', (data) => {
+					emitter.emit('ref', data);
+			});
+			browserJSONStream.on('end', () => emitter.emit('end'));
+			browserJSONStream.on('error', (error) => emitter.emit('error', error));
+			stream.pipe(browserJSONStream);
+	}
+
+		else if (typeof stream.pipe === 'function') {
+			// On node.js
+			console.log('Parsing JSON with node.js library')
+			const nodeJSONStream = JSONStream.parse('*')
+			nodeJSONStream
+			.on('data', ref => emitter.emit('ref', {
+				recNumber: recNumber++,
+				...ref,
+			}))
+			.on('end', ()=> emitter.emit('end'))
+			.on('error', emitter.emit.bind('error'));
+			stream.pipe(nodeJSONStream)
+		}
+
+		else {
+			console.error('Error determining if on browser or node')
+		}
 	});
 
 	return emitter;
@@ -54,7 +112,7 @@ export function writeStream(stream, options) {
 			stream.write(']');
 			return new Promise((resolve, reject) =>
 				stream.end(err => err ? reject(err) : resolve())
-			);
-		},
-	};
+		);
+	},
+};
 }
